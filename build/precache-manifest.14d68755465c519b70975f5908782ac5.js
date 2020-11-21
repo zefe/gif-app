1d0b76e31245222eda417560b6a09a0b1fd69f7c
zefe <zefetorres@icloud.com>
2020-11-20 18:02:36 -0600

    add build

---
+++ build/precache-manifest.14d68755465c519b70975f5908782ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfe7e06c1f3d1de9d4189448f60b1a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "571395d0cfb4b15e6361",
+    "url": "/static/css/main.ad3e838e.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571395d0cfb4b15e6361",
+    "url": "/static/js/main.b0242762.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
